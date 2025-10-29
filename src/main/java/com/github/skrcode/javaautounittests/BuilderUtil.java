dba3b89085029162b1fbd82be1c54d3c48b39c9e
Suraj Rajan <surajkrajan95@gmail.com>
2025-10-29 19:38:56 +0530

    License

--- src/main/java/com/github/skrcode/javaautounittests/BuilderUtil.java
+++ src/main/java/com/github/skrcode/javaautounittests/BuilderUtil.java
@@ -1,3 +1,7 @@
+// Copyright © 2025 Suraj Rajan / JAIPilot
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, you can obtain one at https://mozilla.org/MPL/2.0/.
+
 package com.github.skrcode.javaautounittests;
 
 import com.github.javaparser.JavaParser;
